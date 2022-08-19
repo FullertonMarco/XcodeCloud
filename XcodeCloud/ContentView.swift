2b6257d62f6733f7eec9ab587456f7c71120dae0
Marco <marco.chen@fullerton.com.tw>
2022-08-19 10:28:28 +0800

    Initial Commit

---
+++ XcodeCloud/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeCloud
+//
+//  Created by Marco on 2022/8/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeCloud/XcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudApp.swift
+//  XcodeCloud
+//
+//  Created by Marco on 2022/8/19.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
